16c5e405a4272d812e0dd76369cb8fb9d8457e90
jaroslaw-blazkow <jaroslaw.blazkow@gmail.com>
2024-12-01 23:25:51 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,32 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var (first, second) = input
+                .map {
+                    val (first, second) = it.split("\\s+".toRegex()).map(String::toInt)
+                    first to second
+                }
+                .let { pairs -> pairs.map { it.first } to pairs.map { it.second } }
+        first = first.sorted()
+        second = second.sorted()
+        return first.mapIndexed { index, i -> abs(second[index].minus(i)) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (first, second) = input
+                .map {
+                    val (first, second) = it.split("\\s+".toRegex()).map(String::toInt)
+                    first to second
+                }
+                .let { pairs -> pairs.map { it.first } to pairs.map { it.second } }
+        return first.sumOf { f -> second.count { it == f } * f }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
